146b86d0409e6875e59af7486cd67274f935d66a
syedahsanrazabukhari <syedahsanrazabukhari10@gmail.com>
2024-03-27 16:36:22 +0500

    learn operator

---
+++ class-7(operators,/class7.js
@@ -0,0 +1,42 @@
+// operators: 
+// addition:
+var num1 = 6;
+var num2 = 4;
+var sum = num1 + num2;
+console.log(sum);
+console.log(2 + 3);
+// subtraction:
+var num3 = 6;
+var num4 = 4;
+var sub = num1 - num2;
+console.log(sub);
+console.log(2 - 3);
+// multiplication:
+var num5 = 6;
+var num6 = 4;
+var mul = num1 * num2;
+console.log(mul);
+console.log(2 * 3);
+// division:
+var num7 = 6;
+var num8 = 4;
+var div = num1 / num2;
+console.log(div);
+console.log(2 / 3);
+// exponantation(**) (power)
+var num9 = 5;
+var num10 = 3;
+var expona = Math.pow(num9, num10);
+console.log(expona);
+console.log(Math.pow(2, 2));
+// Modules (%) (remainder)
+var num11 = 5;
+var num12 = 2;
+var modules = num11 % num12;
+console.log(modules);
+console.log(9 % 4);
+//  Unary operators (++) (--)
+var num13 = 5;
+var num14 = 3;
+console.log(++num13);
+console.log(--num14);
---
+++ class-7(operators,/class7.ts
@@ -0,0 +1,62 @@
+// operators: 
+// addition:
+
+let num1=6;
+let num2=4;
+let sum= num1 + num2
+console.log(sum)
+console.log(2+3);
+
+// subtraction:
+let num3=6;
+let num4=4;
+let sub= num1 - num2
+console.log(sub)
+ console.log(2-3);
+
+// multiplication:
+let num5=6;
+let num6=4;
+let mul= num1 * num2
+console.log(mul)
+console.log(2*3);
+
+// division:
+let num7=6;
+let num8=4;
+let div= num1 / num2
+console.log(div)
+console.log(2/3);
+
+// exponantation(**) (power)
+let num9 = 5;
+let num10 = 3;
+let expona = num9 ** num10;
+console.log(expona)
+console.log(2**2)
+
+// Modules (%) (remainder)
+let num11=5
+let num12=2
+let modules = num11 % num12;
+console.log(modules)
+console.log( 9 % 4)
+
+//  Unary operators (++) (--)
+// in pre increment and decrement first update vakue and then return
+// pre-increment
+let num13=5;
+console.log(++num13);
+
+// pre-decreament
+let num14=3;
+console.log(--num14);
+// in post increment and decrement first return vakue and then update
+// post-increment
+let num15=5;
+console.log(num15++);
+
+// post-decreament
+let num16=3;
+console.log(num16--);
+
